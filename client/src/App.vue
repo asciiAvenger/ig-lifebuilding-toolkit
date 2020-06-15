906096f15627295a30810118640c686d7ea620e6
Philipp Alber <philipp_j.alber@yahoo.com>
2020-06-15 23:46:49 +0200

    added rudimentary app-bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <v-app>
-    
+    <nav>
+      <Navbar />
+    </nav>
 
-    <v-content>
-      
-    </v-content>
+    <v-main>
+
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
   name: 'App',
   components: {
+    Navbar
   },
   data() {
+    return {
 
-  },
+    }
+  }
 };
 </script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app-bar app fixed>
+        <v-toolbar-title>
+            <span class="toolbar-bracket">[</span>IG Lifebuilding Toolkit<span class="toolbar-bracket">]</span>
+        </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.toolbar-bracket {
+    font-weight: lighter;
+}
+</style>
